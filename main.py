984033038274a93e2d058035a5e93d3bce602e74
Franck Lavisse <franck@alka.app>
2019-07-17 20:41:32 +0200

    Plot data to show a map of NYC with taxis

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd 
+import pandas as pd
+from vizualisation import plot_lat_long
 
 # found dataset on Kaggle
 df = pd.read_csv('NYC_taxi.csv', parse_dates=['pickup_datetime'], nrows=500000)
@@ -11,23 +12,16 @@ nyc_max_longitude = -73.75
 nyc_min_latitude = 40.63
 nyc_max_latitude = 40.85
 
-# main airport of NYC
-landmarks = {
-    'JFK Airport': (-73.78, 40.643),
-    'Laguardia Airport': (-73.87, 40.77),
-    'Midtown': (-73.98, 40.76),
-    'Lower Manhattan': (-74.00, 40.72),
-    'Upper Manhattan': (-73.94, 40.82),
-    'Brooklyn': (-73.95, 40.66)
-}
-
-df2 = df.copy(deep=True) # we never want to overwrite the original dataframe
-
 def data_cleaning():
+    dfcopy = df.copy(deep=True)
     for long in ['pickup_longitude', 'dropoff_longitude']:
-        df2 = df2[(df2[long] > nyc_min_longitude) & (df2[long] < nyc_max_longitude)]
+        dfcopy = dfcopy[(dfcopy[long] > nyc_min_longitude) &
+                        (dfcopy[long] < nyc_max_longitude)]
     for lat in ['pickup_latitude', 'dropoff_latitude']:
-        df2 = df2[(df2[lat] > nyc_min_latitude) & (df2[lat] < nyc_max_latitude)]   
-    return df2
+        dfcopy = dfcopy[(dfcopy[lat] > nyc_min_latitude) &
+                        (dfcopy[lat] < nyc_max_latitude)]
+    return dfcopy
 
 df2 = data_cleaning()
+
+plot_lat_long(df2)
---
+++ vizualisation.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+
+# main airports of NYC
+landmarks = {
+    'JFK Airport': (-73.78, 40.643),
+    'Laguardia Airport': (-73.87, 40.77),
+    'Midtown': (-73.98, 40.76),
+    'Lower Manhattan': (-74.00, 40.72),
+    'Upper Manhattan': (-73.94, 40.82),
+    'Brooklyn': (-73.95, 40.66)
+}
+
+def draw_landmarks(ax):
+    for landmark in landmarks:
+        ax.plot(landmarks[landmark][0], landmarks[landmark][1],
+                 '*', markersize=15, alpha=1, color='r'
+                 )
+        ax.annotate(landmark,
+                     (landmarks[landmark][0] + 0.005,
+                      landmarks[landmark][1] + 0.005),
+                     color='r')
+
+
+def plot_lat_long(df):
+    plt.subplots(2, 1, figsize=(12, 12))
+
+    ax1=plt.subplot(2, 1, 1)
+    ax2=plt.subplot(2, 1, 2)
+    draw_landmarks(ax1)
+    draw_landmarks(ax2)
+
+    ax1.plot(
+        list(df.pickup_longitude),
+        list(df.pickup_latitude),
+        '.', markersize=1)
+    ax1.set_title('Pickup Locations in NYC')
+    ax1.grid(None)
+    ax1.set_xlabel("Latitude")
+    ax1.set_ylabel("Longitude")
+
+    ax2.plot(
+        list(df.dropoff_longitude),
+        list(df.dropoff_latitude),
+        '.', markersize=1)
+    ax2.set_title('Dropoff Locations in NYC')
+    ax2.grid(None)
+    ax2.set_xlabel("Latitude")
+    ax2.set_ylabel("Longitude")
+
+    plt.show()
